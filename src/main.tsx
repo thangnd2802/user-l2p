5861651b2d4628fd3301fc956090b257912998a5
victor <thangnd2802@gmail.com>
2025-03-31 15:45:32 +0700

    u

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 
-const basename = "/REPO_NAME";
+const basename = "/user-l2p";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
